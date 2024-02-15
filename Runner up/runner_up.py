ef1d717937f0ae311259b4fcd8b16f94c6fb619e
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-02-15 18:43:52 +0530

    Update runner_up.py

--- Runner up/runner_up.py
+++ Runner up/runner_up.py
@@ -1,1 +1,13 @@
-
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    max1 = max(arr)
+    i = 0
+    h = len(arr)
+    while i < h:
+        if arr[i] == max1:
+            arr.remove(max1)
+            h-=1
+            continue
+        i+=1
+    print(max(arr))
