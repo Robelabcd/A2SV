40bd77d73667cfdf153b67e949564621fdb40a92
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-02-29 19:15:26 +0530

    Time: 36 ms (65.58%), Space: 16.4 MB (92.44%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,15 +1,11 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        dict = {} 
-        for i in range(0, len(nums)): 
-            if (nums[i] in dict): 
-                dict[nums[i]] += 1
-            else: 
-                dict[nums[i]] = 1
-        # initialize result 
-        res = 0
-        for key in dict: 
-            if (dict[key] >= 2): 
-                res += ((dict[key] * (dict[key] - 1)) // 2) 
-        return res
+        sum = 0
+        #nums.sort()
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] == nums[j]:
+                    sum +=1
+        return sum
+        
         
