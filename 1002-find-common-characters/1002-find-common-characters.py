d3957711116e2042224cc90f593f53d3feacf049
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-03-03 00:33:06 +0530

    Time: 50 ms (56.65%), Space: 16.7 MB (84.47%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,28 +1,15 @@
-# class Solution:
-#     def commonChars(self, words: List[str]) -> List[str]:
-        
-#         str1 = list(words[0])      #assume str1 contains the duplicate 
-
-#         for i in range(1, len(words)):
-#             str2 = words[i]       #stores the next word each time
-#             for x in str1:
-#                 if not(x in str2):
-#                     str1.remove(x)
-
-        
-
-
-#         return str1
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        common_word = []
-        first_word = words[0]
+        w = Counter(words[0])
+        n = len(words)
+        for i in range(1, n):
+            t = Counter(words[i])
+            w = w & t
         
-        for char in set(first_word):
-            count = first_word.count(char)
-            for word in words[1:]:
-                count = min(count, word.count(char))
-            
-            common_word.extend([char] * count)
-
-        return common_word
+        ans = []
+        for k in w.keys():
+            for i in range(w[k]):
+                ans.append(k)
+        
+        return ans
+        
