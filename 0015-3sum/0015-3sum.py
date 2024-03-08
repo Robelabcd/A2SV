90ff7d1ab6127f1f01b22357d76a076177dbd18c
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-03-08 14:13:31 +0530

    Time: 731 ms (46.71%), Space: 20.7 MB (59.07%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,50 @@
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         res = []
+#         for i in range(len(nums)-2):
+            
+#             j, k = i + 1, i + 2
+#             while k < len(nums):
+
+#                 if (nums[i] + nums[j] + nums[k]) == 0:
+#                     res.append([nums[i], nums[j], nums[k]])
+
+#                 j += 1
+#                 k += 1
+
+#         for i in range(len(res)-1, -1, -1):
+#             print(res[i])
+
+#         return res
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()  
+
+        for i in range(len(nums)-2):
+            
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            j, k = i + 1, len(nums) - 1
+
+            while j < k:
+                total_sum = nums[i] + nums[j] + nums[k]
+
+                if total_sum == 0:
+                    res.append([nums[i], nums[j], nums[k]])
+                    
+                    while j < k and nums[j] == nums[j+1]:
+                        j += 1
+                    
+                    while j < k and nums[k] == nums[k-1]:
+                        k -= 1
+                    j += 1
+                    k -= 1
+                elif total_sum < 0:
+                    j += 1
+                else:
+                    k -= 1
+
+        return res
