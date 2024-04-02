f75127d37af1472fde5d6aa7cd3ad0df95fc7afe
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-04-02 20:13:51 +0530

    Time: 658 ms (5.03%), Space: 30.9 MB (63.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,30 +1,38 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+        pref_sum = [0] * (len(nums)+1)
+        pref_sum[0] = 0
+        for i in range(1, len(pref_sum)):
+            pref_sum[i] = pref_sum[i-1] + nums[i-1]
+        print(pref_sum)
+        # [0, -2, -1, -4, 0, -1, 1, 2, -3, 1]
+
+        minn = inf
+        min_arr = []
+        for i in range(0, len(pref_sum)):
+            minn = min(minn, pref_sum[i])
+            min_arr.append(minn)
+        print(min_arr)
+        # [0, -2, -2, -4, -4, -4, -4, -4, -4, -4]
+
+        maxx = -inf
+        for i in range(1, len(pref_sum)):
+            summ = pref_sum[i] - min_arr[i-1]
+
+            maxx = max(summ, maxx)
+
+        return maxx
+
         
-        '''
-        [8,-19,5,-4,20]
-                
-
-        '''
 
         
-        lp, rp = 0, 0
-        summ = 0
-        res = max(nums)
-        for rp in range(len(nums)):
-            summ += nums[rp]
-            if summ <= nums[rp]:
-                
-                lp = rp
-
-                summ = nums[lp]
-
-            res = max(res, summ)
-
         
 
-        return res
 
+            
 
 
 
