8a3dcdc970e92170e54e05fdf1d70eaf77a5065a
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-04-08 17:19:31 +0530

    Time: 38 ms (72.82%), Space: 16.5 MB (59.9%) - LeetHub

---
+++ 0098-number-of-students-unable-to-eat-lunch/0098-number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def countStudents(self, students, sandwiches):
+        
+        zeros = students.count(0)
+        ones = len(students) - zeros
+        for sandwich in sandwiches:
+            if sandwich == 0:
+                if zeros > 0:
+                    zeros -= 1
+                else:
+                    break
+            elif sandwich == 1:
+                if ones > 0:
+                    ones -= 1
+                else:
+                    break
+        return zeros + ones   
