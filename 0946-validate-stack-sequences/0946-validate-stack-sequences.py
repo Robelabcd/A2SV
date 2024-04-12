436a7d0a4c147a903d7dd869391a8cb68efc98c4
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-04-12 21:36:33 +0530

    Time: 59 ms (87.94%), Space: 16.8 MB (16.44%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.py
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.py
@@ -1,6 +1,34 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        '''
         
+        pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+
+        is it possible to have this sequence by doing and popping the elements
+
+        let's create a test stack to see the possiblities
+
+        test = []
+        [1] push elements from pushed stack
+        .
+        .
+        .
+        [1, 2, 3, 4] ---> now test == popped
+        so pop 4 from test and check for next popped element = 5
+
+        [1, 2, 3] 3 != 5 --->nope so continue pushing
+
+        [1, 2, 3, 5] test == popped
+        so pop from test
+        .
+        .
+        .
+        [1] test == popped
+
+        now test = [] and also we reached the last element of popped
+        so return True
+        
+        '''
 
         test = []
 
