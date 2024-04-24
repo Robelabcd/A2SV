309e96683299d81f75dfe8d08c5fc568518f3392
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-04-24 23:27:21 +0530

    Time: 1109 ms (8.02%), Space: 25.9 MB (72.39%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,8 +1,8 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         
-        inc = deque() 
-        dec = deque()
+        inc = [] 
+        dec = []
 
         left = 0
         res = 0
@@ -33,10 +33,10 @@ class Solution:
             while nums[inc[0]] - nums[dec[0]] > limit:
 
                 if inc[0] == left:
-                    inc.popleft()
+                    inc.pop(0)
 
                 if dec[0] == left:
-                    dec.popleft()
+                    dec.pop(0)
 
                 left += 1
 
@@ -44,4 +44,59 @@ class Solution:
 
 
         return res
+
+
+
+    '''
+        Approch 2 - using deque
+
+
+
+        class Solution:
+            def longestSubarray(self, nums: List[int], limit: int) -> int:
+                
+                inc = deque() 
+                dec = deque()
+
+                left = 0
+                res = 0
+
+                for i, val in enumerate(nums):
+
+                    while inc and nums[inc[-1]]<val:
+                        inc.pop()
+
+                    inc.append(i)
+
+                    
+                    2
+                    
+                    
+                    
+
+                    while dec and nums[dec[-1]] > val:
+
+                        dec.pop()
+
+                    dec.append(i)
+
+                    
+                    1 2
+                    
+                    
+                    while nums[inc[0]] - nums[dec[0]] > limit:
+
+                        if inc[0] == left:
+                            inc.popleft()
+
+                        if dec[0] == left:
+                            dec.popleft()
+
+                        left += 1
+
+                    res = max(res, i - left + 1)
+
+
+                return res
+    '''
 
