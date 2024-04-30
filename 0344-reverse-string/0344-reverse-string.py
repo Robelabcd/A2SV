978ea00285509952abf407c8f1075e0248669754
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-04-30 21:32:44 +0530

    Time: 167 ms (70.65%), Space: 20.7 MB (93.47%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,4 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        return s.reverse()
+        
+        n = len(s)
+
+        for i in range(n//2):
+
+            s[i], s[n-i-1] = s[n-i-1], s[i]
+
+
