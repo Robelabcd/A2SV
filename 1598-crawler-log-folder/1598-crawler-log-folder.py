d29f25dbe64641d619536225da1fcd35f87ddfa2
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-05-28 15:37:52 +0530

    Time: 51 ms (45.67%), Space: 16.6 MB (96.01%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -1,14 +1,30 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        stack = []
+        '''
+        conditions:
+        ../   ----> to one step up to the main folder
+        ./    ----> stay there
+        x/    ----> move to child 'x'
 
-        for element in logs:
-            if element.startswith('../'):
+        Use stack to contain the child folder and return the how many childrena re there in the stack to go back to the main folder
+        '''
+
+        stack = [] #to store the folders
+
+        #go through the string/log
+        for i in range(len(logs)):
+
+            #chack the conditions
+            if logs[i] == "./":
+                continue   #no need to do any operation
+
+            elif logs[i] == "../":
                 if stack:
-                    stack.pop()
-            elif element.startswith('./'):
-                continue
+                    stack.pop()  #one step up to the main folder
+                else:
+                    continue
             else:
-                stack.append(element)
 
-        return len(stack)
+                stack.append(logs[i])  #otherwise add the child folders in the stack
+
+        return len(stack) #number of child folder
