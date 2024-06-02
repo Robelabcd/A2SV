dbc241cbecbf525763d0f7a59d3761f51ca8ee17
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-06-02 21:29:15 +0530

    Time: 179 ms (10.43%), Space: 22.3 MB (5.16%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,13 +1,39 @@
 class Solution:
+
+
+
+    def reverse(self, initial, end, s):
+
+        #base case
+        if initial >= end:
+            return
+
+        s[initial], s[end] = s[end], s[initial]
+
+        self.reverse(initial+1, end - 1, s)
+
+
+
+
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
         
+        # n = len(s)
+
+        # for i in range(n//2):
+
+        #     s[i], s[n-i-1] = s[n-i-1], s[i]
+
+        # Recurssion
+
         n = len(s)
+        end = n - 1
+        # i = 0 --> initial
 
-        for i in range(n//2):
+        self.reverse(0, end, s)
 
-            s[i], s[n-i-1] = s[n-i-1], s[i]
+
 
 
