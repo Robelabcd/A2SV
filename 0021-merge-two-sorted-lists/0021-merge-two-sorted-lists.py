9f85f81560f32de333fa861f1ddd053f50327ef9
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-06-04 00:56:47 +0530

    Time: 40 ms (45.1%), Space: 16.4 MB (99.32%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -6,32 +6,18 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
-        #create a dummy node to avoid the edge case at the beginning
+        #base cases 
+        if not list1:
+            return list2
 
-        dummy = ListNode(0)
-        cur = dummy
-
-        #start iterating through the linked lists
-        #by comparing the value
-
-        while list1 and list2:
-
-            if list1.val < list2.val:
-                cur.next = list1
-                list1 = list1.next
-
-            else:
-                cur.next = list2
-                list2 = list2.next
-        
-            #update the cur
-            cur = cur.next
-
-        #what if the lists are not equal in size
-        if list1:
-            cur.next = list1
-        elif list2:
-            cur.next = list2
+        if not list2:
+            return list1
 
         
-        return dummy.next
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
