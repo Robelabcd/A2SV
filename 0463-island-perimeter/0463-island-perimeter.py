5febbc7c7b337c12942caefaa3b811e0d0a67b31
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-06-18 20:51:15 +0530

    Time: 514 ms (6.4%), Space: 17.9 MB (18.74%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,16 +1,17 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
+
         def dfs(i, j, seen):
-            if i < 0 or i > row-1 or j < 0 or j > col-1 or grid[i][j] == 0:
-                return 1
+            if i < 0 or i > row-1 or j < 0 or j > col-1 or grid[i][j] == 0:  #not inbound or adjecent to water body
+                return 1  #conisder
            
-            if seen[i][j]:
+            if seen[i][j]:  #bounded by land or seen before 
                 return 0
             
+
             seen[i][j] = 1
             
-            
-            return dfs(i-1, j, seen) + dfs(i+1, j, seen) + dfs(i, j+1, seen) + dfs(i, j-1, seen)
+            return dfs(i-1, j, seen) + dfs(i+1, j, seen) + dfs(i, j+1, seen) + dfs(i, j-1, seen)  #perimeter
         
         
         row, col = len(grid), len(grid[0])
@@ -19,5 +20,5 @@ class Solution:
         
         for x in range(row):
             for y in range(col):
-                if grid[x][y] == 1:
+                if grid[x][y] == 1:  #the one end of the land is found --no need of the water body
                     return dfs(x, y, seen)                
