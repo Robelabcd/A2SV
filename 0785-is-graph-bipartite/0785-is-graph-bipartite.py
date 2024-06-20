60ac3dbd875800cba9b3db628f3ccc52256f2d13
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-06-20 19:57:34 +0530

    Time: 136 ms (93.03%), Space: 17.1 MB (29.07%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        n = len(graph)
+        visited = [-1] * n
+
+        def helper(node, assign):
+            visited[node] = assign
+
+            for neighbor in graph[node]:
+                if visited[neighbor] == -1:
+                    if not helper(neighbor, 1-assign):
+                        return False
+
+                elif visited[neighbor] == visited[node]:
+                    return False
+
+            return True
+
+        for node in range(n):
+
+            if visited[node] == -1:
+
+                if not helper(node, 0):
+                    return False
+
+        return True
