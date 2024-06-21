285bba5cd0619873ba26a6672d0a4358491ec9a3
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-06-21 12:37:42 +0530

    Time: 260 ms (38.08%), Space: 18.9 MB (73.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,35 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+
+        def inbound(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
+                return False
+
+            return True 
                 
         def helper(i, j):
-            # check inbound
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+            # check boundary and water territory
+            if (not inbound(i, j)) or grid[i][j] != '1':
                 return
             
             grid[i][j] = '0' #visited
             
-            
+            '''
             helper(i+1, j) #down
             helper(i-1, j) #up
             helper(i, j+1) #left
             helper(i, j-1) #right
-        
+            '''
+            directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            for row, col in directions:
+                helper(i+row, j+col)
+
         num = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 #find land
                 if grid[i][j] == '1':
-                    num += 1
                     helper(i, j)
+                    num += 1
         
         return num
