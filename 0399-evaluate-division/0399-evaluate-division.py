518c595322d52c1260e0c9909fe722eb1c0572ad
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-06-24 02:00:49 +0530

    Time: 43 ms (12.73%), Space: 16.6 MB (39.45%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,8 +1,6 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         
-
-
         graph = defaultdict()
 
         for index, relation in enumerate(equations):
