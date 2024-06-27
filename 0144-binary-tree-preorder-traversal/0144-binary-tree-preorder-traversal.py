4d1e5c49c7da3cf70b019331ed47e3a8e3f138c2
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-06-27 18:18:40 +0530

    Time: 35 ms (55.79%), Space: 16.4 MB (92.07%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -7,6 +7,34 @@
 class Solution:
     
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        #iterative approach
+
+        res = [] 
+        
+        stack = []
+
+        cur = root
+
+        while cur or stack:
+
+            if cur:
+                #append the root and store the right nodes along the way
+                res.append(cur.val)
+                stack.append(cur.right)
+                cur = cur.left              #and go to the left node 
+        
+            else:
+
+                cur = stack.pop()    #if cur is null, we need to pop back to previous nodes  
+
+
+        return res
+        
+        
+        '''
+        recursive approach 
+
         res = []
 
         def helper(root):
@@ -24,5 +52,7 @@ class Solution:
         helper(root)
 
         return res
+
+        '''
         
     
