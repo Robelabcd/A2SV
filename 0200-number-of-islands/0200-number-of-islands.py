7f813534a6c6d761f2022a5c4f1d68bc39b712d3
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-06-28 15:12:00 +0530

    Time: 275 ms (28.06%), Space: 26.9 MB (15.59%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,5 +1,6 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        visited = set()
 
         def inbound(i, j):
             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
@@ -12,7 +13,10 @@ class Solution:
             if (not inbound(i, j)) or grid[i][j] != '1':
                 return
             
-            grid[i][j] = '0' #visited
+            if (i, j) in visited:
+                return
+
+            visited.add((i, j))
             
             '''
             helper(i+1, j) #down
@@ -28,7 +32,7 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 #find land
-                if grid[i][j] == '1':
+                if grid[i][j] == '1' and (i, j) not in visited:
                     helper(i, j)
                     num += 1
         
