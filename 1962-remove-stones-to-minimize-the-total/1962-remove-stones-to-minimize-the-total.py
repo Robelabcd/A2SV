da672596ef8c99d9ce2ce14a56655e8f144945e9
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-07-26 22:33:30 +0530

    Time: 1364 ms (14.23%), Space: 31.5 MB (6.32%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
@@ -1,5 +1,27 @@
 class Solution:
     def minStoneSum(self, piles: List[int], k: int) -> int:
+        
+        
+        #bruteforce approach
+
+        # - find max_ each time:
+            # divide by two - k times
+
+        # - add all the nums and return
+        '''
+        for _ in range(k):
+
+            maxx = max(piles)
+
+            index = piles.index(maxx)
+
+            piles[index] = piles[index] - piles[index]//2
+
+        
+        return sum(piles)
+
+        Time complexity: O(k*n)
+        '''
         res = sum(piles)
         heapp = []
         for x in piles:
@@ -12,4 +34,3 @@ class Solution:
             heapq.heappush(heapp, maxx + reduced)
 
         return res
-     
