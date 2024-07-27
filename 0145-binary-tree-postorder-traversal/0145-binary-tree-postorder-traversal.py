0dd6ac977bfeaf5e1f7f4199c8179ec0a1c171d8
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-07-27 12:18:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -29,7 +29,7 @@ class Solution:
         '''
         # Iterative approach - using flag
         # Post order ---> left -> right -> root
-
+        '''
         stack = [(root, False)]
         res = []
 
@@ -46,6 +46,70 @@ class Solution:
                     stack.append((node.left, False))
 
         return res
+        '''
+        
+        #iterative - method 2
+        
+        '''
+        using two stacks:
+        - one to store the value
+        - the other to traverse
+        '''
+
+        stack1 = [root]  #to traverse
+        stack2 = []      #to store value along the way
+        res = []
+
+        while stack1:
+
+            cur = stack1.pop()
+            if cur:
+                stack2.append(cur.val)  #NB: we're storing in the reverse order 
+                                            #since root is the last element in postorder
+            
+            if cur and cur.right:
+                stack1.append(cur.right)
+
+            if cur and cur.left:
+                stack1.append(cur.left)
+
+
+        #we stored the values from back to front(root -> leftmost) - reversePostorder
+
+        while stack2:
+            node_val = stack2.pop()
+            res.append(node_val)
+
+
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
