6b72acb891c0d78ccd8094895d7c162684e4e053
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-08-05 15:56:08 +0530

    Time: 43 ms (8.36%), Space: 16.5 MB (34.35%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -6,17 +6,20 @@ class Solution:
         
         res = []
         dicts = {"2": "abc","3": "def","4": "ghi","5": "jkl","6": "mno","7": "pqrs","8": "tuv","9": "wxyz"}
+        combo = []
 
-        def combos_function(i, combo):
-            if i >= len(digits):
-                res.append(combo)
+        def combos_function(i):
+            if i == len(digits):
+                res.append(''.join(combo))
                 return
 
-            for c in dicts[digits[i]]:
-                combos_function(i + 1, combo + c)
+            for letter in dicts[digits[i]]:
+                combo.append(letter)
+                combos_function(i + 1)
+                combo.pop()
 
         if digits:
-            combos_function(0, "")
+            combos_function(0)
 
         return res
   
