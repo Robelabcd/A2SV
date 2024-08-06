46734a63f6be18704ca0fff114124bb435498aac
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-08-06 19:57:57 +0530

    Time: 45 ms (39.84%), Space: 16.6 MB (11.76%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,16 +1,19 @@
 class Solution:
     def fib(self, n: int) -> int:
         
+        memo = {}
         def rec(n):
             
-            if n <= 0:
-                return 0
+            if n is 0 or n is 1:
+                return n
 
-            if n == 1:
-                return 1
+            if n in memo:
+                return memo[n]
 
-
-            return rec(n-1) + rec(n-2)
+            else:
+                memo[n] = rec(n-1) + rec(n-2)
 
         
+            return memo[n]
+
         return rec(n)
