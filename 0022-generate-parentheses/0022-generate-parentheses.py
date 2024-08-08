48831a93462c987e3f2204418bf5229ed840ca69
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-08-08 10:22:03 +0530

    Time: 26 ms (98.49%), Space: 16.9 MB (38.52%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,19 +1,39 @@
 class Solution:
-    def backtrack(self, close_p, open_p, stack, result):
-        if close_p == 0:
-            result.append(''.join(stack))
+    def generateParenthesis(self, n: int) -> List[str]:
 
-        if open_p:
-            stack.append('(')
-            self.backtrack(close_p, open_p - 1, stack, result)
-            stack.pop()
+        '''
+        two options:
+            - use open parentheses as long as num_open_p <= n
 
-        if close_p > open_p:
-            stack.append(')')
-            self.backtrack(close_p - 1, open_p, stack, result)
-            stack.pop()
+            - use close parentheses if num_open_p > num_close_p
+        
+        '''
+        
 
-    def generateParenthesis(self, n):
-        stack, result = [], []
-        self.backtrack(n, n, stack, result)
-        return result
+        res, sol = [], []
+
+        def backtrack(open_p, close_p):
+
+            #base case: if solution includes all parentheses 
+            if len(sol) == 2*n:
+                res.append(''.join(sol))
+
+
+            #open_parentheses
+            if open_p < n:
+                sol.append('(')
+                backtrack(open_p+1, close_p)
+                sol.pop()
+
+            
+            #close parentheses
+            if close_p < open_p:
+                sol.append(')')
+                backtrack(open_p, close_p + 1)
+                sol.pop()
+            
+        
+        backtrack(0, 0)
+
+        return res
+
