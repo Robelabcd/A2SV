faf9a001be7fbd7bd0bc72fe4d10b668ff5e69ae
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-08-09 15:01:58 +0530

    Time: 43 ms (5.99%), Space: 16.4 MB (55.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,81 @@
 class Solution:
-    @cache
     def climbStairs(self, n: int) -> int:
         
-        def count(n):
-            if n == 0:
+        '''
+        stairs - ways to get there
+
+        1 - 1 = 1 way
+        2 - 1+1, 2 = 2 ways
+        3 - 1+1+1, 1+2, 2+1 = 3 ways
+        4 - 1+1+1+1, 1+1+2, 1+2+1, 2+1+1, 2+2 = 5ways
+
+        recurrence relation => T(n) = T(n-1) + T(n-2)
+                e.g for n = 4: T(4) = T(3) + T(2) = 3 + 2 = 5 ways 
+        '''
+        
+        
+        #Problem: TLE
+        #recursive solution
+        '''
+        def rec(n):
+
+            if n == 1:
                 return 1
 
-            if n < 0:
-                return 0
+            if n == 2:
+                return 2
 
-            return self.climbStairs(n-1) + self.climbStairs(n-2)
+            return rec(n-1) + rec(n-2)
 
-        return count(n)
+        
+        return rec(n)
+        
+        '''
+
+
+        #Top-down approach / memoization
+
+
+        memo = {1:1, 2:2}
+        def dp(n):
+
+            if n in memo:
+                return memo[n]
+
+            else:
+                memo[n] = dp(n-1) + dp(n-2)
+
+
+            return memo[n]
+
+        
+        return dp(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
