e7633bfe37e0105e8ea4cba9ee2fc48db91cc5f7
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-08-16 19:14:23 +0530

    Time: 5618 ms (5%), Space: 145.7 MB (5.01%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,6 +1,6 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        
+        '''
         n=len(nums)
         dp=[[-1 for j in range(n+1)] for i in range(n)]
         
@@ -24,4 +24,24 @@ class Solution:
             return dp[index][previous+1]
         
         
-        return backtrack(0,-1)   
+        return backtrack(0,-1) 
+
+        '''
+        
+        #bottom up approach
+        
+        n=len(nums)
+        dp=[[0 for j in range(n+1)] for i in range(n+1)]
+        
+        for index in range(n-1,-1,-1):
+            for previous in range(index-1,-2,-1): 
+                
+                not_take=0+dp[index+1][previous+1]
+                
+                take=0
+                if previous==-1 or nums[index]>nums[previous]:
+                    take=1+dp[index+1][index+1]
+                
+                dp[index][previous+1]=max(take,not_take)
+        
+        return dp[0][0]  
