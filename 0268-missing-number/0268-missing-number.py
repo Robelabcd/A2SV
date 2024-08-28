1f64ef432a1915505df785506eb83460ed931062
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-08-29 00:23:24 +0530

    Time: 123 ms (22.21%), Space: 18.3 MB (10.77%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,23 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-
+        '''
         N = len(nums)
         num_set = set(nums)
 
         for i in range(N+1):
             if not(i in num_set):
                 return i
-        
+        '''
+
+        # maxx = max(nums)
+        res = 0
+        new_nums = nums
+        for i in range(len(nums)+1):
+            new_nums.append(i)
+
+        print(nums)
+        for num in new_nums:
+
+            res = res ^ num
+
+        return res
