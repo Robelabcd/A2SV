015e93dc31019dd7dcb3ec33ce2bd69cd2a22075
Robel <99393795+Robelabcd@users.noreply.github.com>
2024-10-01 20:16:01 +0530

    Time: 486 ms (52.46%), Space: 19.9 MB (38.24%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -7,9 +7,11 @@ class Solution:
             for j in words[i]:
                 bitmask |= 1 << (ord(j) - ord('a'))
             arr[i] = bitmask
+        
         max_product = 0  
         for i in range(len(words)):
             for j in range(1, len(words)):
                 if arr[i] & arr[j] == 0:
                     max_product = max(max_product, len(words[i]) * len(words[j]))
+        
         return max_product
